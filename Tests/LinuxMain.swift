6b7b355cb12a7dfbff14ed3dc8ceb8cf831b9fac
Jonathan Downing <jd@jonathandowning.uk>
2019-10-01 13:01:50 +0100

    Remove Linux Tests

--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import METARTests
-
-XCTMain([
-    testCase(METARTests.allTests),
-])
--- Tests/METARTests/METARTests.swift
+++ Tests/METARTests/METARTests.swift
@@ -3,13 +3,13 @@ import XCTest
 
 @available(iOS 10.0, macOS 10.12, tvOS 10.0, watchOS 3.0, *)
 final class METARTests: XCTestCase {
-    
+
     func testDatelessMETAR() {
         let metarString = "KRDU 22007KT 9SM FEW080 FEW250 22/20 A3004 RMK AO2 SLP166 60000 T02170200 10222 20206 53022"
         let metar = METAR(rawMETAR: metarString)
         XCTAssertNil(metar)
     }
-    
+
     func testSimpleMETAR() {
         let metarString = "KRDU COR 281151Z AUTO 22007KT 9SM SCT040TCU FEW080 FEW250 22/20 SHRA BLU A3004 NOSIG RMK AO2 SLP166 60000 T02170200 10222 20206 53022"
         let metar = METAR(rawMETAR: metarString)
@@ -35,10 +35,10 @@ final class METARTests: XCTestCase {
             metarString: metarString,
             flightRules: .vfr
         )
-        
+
         XCTAssertEqual(metar, testMETAR)
     }
-    
+
     func testSlashesInBetweenCloudTypeAndHeight() {
         let metarString = "EGGD 251250Z AUTO 25016G27KT 220V280 9999 BKN019///TCU 11/11 Q1013"
         let metar = METAR(rawMETAR: metarString)
@@ -64,10 +64,10 @@ final class METARTests: XCTestCase {
             metarString: metarString,
             flightRules: .mvfr
         )
-        
+
         XCTAssertEqual(metar, testMETAR)
     }
-    
+
     func testTEMPO() {
         let metarString = "UUDD 261100Z 25007MPS 200V290 9999 BKN026 14/08 Q0997 R88/290095 TEMPO 28012G18MPS 1500 TSRA BKN015CB RMK TEST REMARKS"
         let metar = METAR(rawMETAR: metarString)
@@ -95,14 +95,14 @@ final class METARTests: XCTestCase {
         )
         XCTAssertEqual(metar, testMETAR)
     }
-    
+
     func testLIFRVisibility() {
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z FEW010 1/8SM")?.flightRules, .lifr)
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z FEW010 1/4SM")?.flightRules, .lifr)
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z FEW010 1/2SM")?.flightRules, .lifr)
         XCTAssertNotEqual(METAR(rawMETAR: "EGGD 121212Z FEW010 1SM")?.flightRules, .lifr)
     }
-    
+
     func testLIFRCeiling() {
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z OVC001 10SM")?.flightRules, .lifr)
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z OVC002 10SM")?.flightRules, .lifr)
@@ -110,33 +110,33 @@ final class METARTests: XCTestCase {
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z OVC004 10SM")?.flightRules, .lifr)
         XCTAssertNotEqual(METAR(rawMETAR: "EGGD 121212Z OVC005 10SM")?.flightRules, .lifr)
     }
-    
+
     func testIFRVisibility() {
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z FEW010 1SM")?.flightRules, .ifr)
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z FEW010 2SM")?.flightRules, .ifr)
         XCTAssertNotEqual(METAR(rawMETAR: "EGGD 121212Z FEW010 3SM")?.flightRules, .ifr)
     }
-    
+
     func testIFRCeiling() {
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z OVC005 10SM")?.flightRules, .ifr)
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z OVC009 10SM")?.flightRules, .ifr)
         XCTAssertNotEqual(METAR(rawMETAR: "EGGD 121212Z OVC010 10SM")?.flightRules, .ifr)
     }
-    
+
     func testMVFRVisibility() {
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z FEW010 3SM")?.flightRules, .mvfr)
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z FEW010 4SM")?.flightRules, .mvfr)
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z FEW010 5SM")?.flightRules, .mvfr)
         XCTAssertNotEqual(METAR(rawMETAR: "EGGD 121212Z FEW010 5 1/8SM")?.flightRules, .mvfr)
     }
-    
+
     func testMVFRCeiling() {
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z OVC010 10SM")?.flightRules, .mvfr)
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z OVC020 10SM")?.flightRules, .mvfr)
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z OVC030 10SM")?.flightRules, .mvfr)
         XCTAssertNotEqual(METAR(rawMETAR: "EGGD 121212Z OVC031 10SM")?.flightRules, .mvfr)
     }
-    
+
     func testVFR() {
         XCTAssertEqual(METAR(rawMETAR: "EKTE 211720Z AUTO 22011KT 9999NDV NCD 11/10 Q1026=")?.flightRules, .vfr)
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z 10SM")?.flightRules, .vfr)
@@ -144,11 +144,11 @@ final class METARTests: XCTestCase {
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z SCT010 9999")?.flightRules, .vfr)
         XCTAssertNil(METAR(rawMETAR: "EGGD 121212Z ///010 10SM")?.flightRules)
     }
-    
+
     func testWind() {
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z 33008G15KT 300V360")?.wind, Wind(direction: 330, speed: Wind.Speed(value: 8, unit: .knots), gustSpeed: Wind.Speed(value: 15, unit: .knots), variation: Wind.Variation(from: 300, to: 360)))
     }
-    
+
     func testVisbility() {
         XCTAssertEqual(METAR(rawMETAR: "EKTE 211720Z AUTO 22011KT 9999NDV NCD 11/10 Q1026=")?.visibility, Visibility(value: 10, unit: .kilometers, greaterThanOrEqual: true))
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z 10SM")?.visibility, Visibility(value: 10, unit: .miles, greaterThanOrEqual: false))
@@ -160,7 +160,7 @@ final class METARTests: XCTestCase {
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z 10SM")?.ceilingAndVisibilityOK, false)
         XCTAssertEqual(METAR(rawMETAR: "EGGD 121212Z 9999")?.ceilingAndVisibilityOK, false)
     }
-    
+
     private func date(day: Int, hour: Int, minute: Int) -> Date {
         let calendar = Calendar(identifier: .gregorian)
         var components = calendar.dateComponents(in: TimeZone(identifier: "UTC")!, from: Date())
@@ -174,18 +174,5 @@ final class METARTests: XCTestCase {
         components.nanosecond = 0
         return components.date!
     }
-    
-    static var allTests = [
-        ("testDatelessMETAR", testDatelessMETAR),
-        ("testSimpleMETAR", testSimpleMETAR),
-        ("testLIFRVisibility", testLIFRVisibility),
-        ("testLIFRCeiling", testLIFRCeiling),
-        ("testIFRVisibility", testIFRVisibility),
-        ("testIFRCeiling", testIFRCeiling),
-        ("testMVFRVisibility", testMVFRVisibility),
-        ("testMVFRCeiling", testMVFRCeiling),
-        ("testVFR", testVFR),
-        ("testWind", testWind),
-        ("testVisbility", testVisbility)
-    ]
+
 }
