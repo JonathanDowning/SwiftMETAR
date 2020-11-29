586573ab8ac46aae64bd0ddb6572c927566fc5d1
Jonathan Downing <jd@jonathandowning.uk>
2020-11-29 09:22:46 -0500

    Simplify tests

--- Tests/METARTests/METARTests.swift
+++ Tests/METARTests/METARTests.swift
@@ -9,56 +9,13 @@ final class METARTests: XCTestCase {
         XCTAssertNil(metar)
     }
 
-    func testSimpleMETAR() throws {
-        try compareMETAR("KRDU COR 281151Z AUTO 22007KT 9SM SCT040TCU FEW080 FEW250 22/20 SHRA BLU A3004 NOSIG RMK AO2 SLP166 60000 T02170200 10222 20206 53022", "KRDU", 28, 11, 51, wind: Wind(direction: 220, speed: .init(value: 7, unit: .knots)), qnh: QNH(value: 30.04, unit: .inchesOfMercury), cloudLayers: [.init(coverage: .scattered, height: .init(value: 4000, unit: .feet), significantCloudType: .toweringCumulus), .init(coverage: .few, height: .init(value: 8000, unit: .feet)), .init(coverage: .few, height: .init(value: 25000, unit: .feet))], visibility: Visibility(value: 9, unit: .miles), weather: [Weather(modifier: .moderate, phenomena: [.showers, .rain])], militaryColourCode: .blue, temperature: Temperature(value: 22, unit: .celsius), dewPoint: Temperature(value: 20, unit: .celsius), automaticStation: true, correction: true, noSignificantChangesExpected: true, remarks: "AO2 SLP166 60000 T02170200 10222 20206 53022", flightRules: .vfr)
-    }
-
     func testMETARs() throws {
-        try compareMETAR("EGGD 121212Z ///010 10SM", "EGGD", 12, 12, 12, cloudLayers: [.init(coverage: .notReported, height: .init(value: 1000, unit: .feet))], visibility: .init(value: 10, unit: .miles))
-        try compareMETAR("EGGD 121212Z ///010 1 1/4SM", "EGGD", 12, 12, 12, cloudLayers: [.init(coverage: .notReported, height: .init(value: 1000, unit: .feet))], visibility: .init(value: 1.25, unit: .miles))
-    }
-
-    func testSlashesInBetweenCloudTypeAndHeight() throws {
-        let metarString = "EGGD 251250Z AUTO 25016G27KT 220V280 9999 BKN019///TCU 11/11 Q1013"
-        let metar = METAR(rawMETAR: metarString)
-        let testMETAR = METAR(
-            identifier: "EGGD",
-            date: date(day: 25, hour: 12, minute: 50),
-            wind: Wind(direction: 250, speed: .init(value: 16, unit: .knots), gustSpeed: .init(value: 27, unit: .knots), variation: .init(from: 220, to: 280)),
-            qnh: QNH(value: 1013, unit: .hectopascals),
-            cloudLayers: [
-                CloudLayer(coverage: .broken, height: .init(value: 1900, unit: .feet), significantCloudType: .toweringCumulus)
-            ],
-            visibility: Visibility(value: 10, unit: .kilometers, greaterThanOrEqual: true),
-            temperature: Temperature(value: 11, unit: .celsius),
-            dewPoint: Temperature(value: 11, unit: .celsius),
-            automaticStation: true,
-            metarString: metarString,
-            flightRules: .mvfr
-        )
-
-        try XCTAssertEqual(XCTUnwrap(metar), XCTUnwrap(testMETAR))
-    }
-
-    func testTEMPO() throws {
-        let metarString = "UUDD 261100Z 25007MPS 200V290 9999 BKN026 14/08 Q0997 R88/290095 TEMPO 28012G18MPS 1500 TSRA BKN015CB RMK TEST REMARKS"
-        let metar = METAR(rawMETAR: metarString)
-        let testMETAR = METAR(
-            identifier: "UUDD",
-            date: date(day: 26, hour: 11, minute: 00),
-            wind: Wind(direction: 250, speed: Wind.Speed(value: 7, unit: .metersPerSecond), gustSpeed: nil, variation: Wind.Variation(from: 200, to: 290)),
-            qnh: QNH(value: 997, unit: .hectopascals),
-            cloudLayers: [CloudLayer(coverage: .broken, height: .init(value: 2600, unit: .feet))],
-            visibility: Visibility(value: 10, unit: .kilometers, greaterThanOrEqual: true),
-            trends: [Forecast(metarRepresentation: METAR(identifier: "UUDD", date: date(day: 26, hour: 11, minute: 00), wind: Wind(direction: 280, speed: Wind.Speed(value: 12, unit: .metersPerSecond), gustSpeed: Wind.Speed(value: 18, unit: .metersPerSecond), variation: nil), cloudLayers: [CloudLayer(coverage: .broken, height: CloudLayer.Height(value: 1500, unit: .feet), significantCloudType: .cumulonimbus)], visibility: Visibility(value: 1500, unit: .meters), weather: [Weather(modifier: .moderate, phenomena: [.thunderstorm, .rain])], metarString: "28012G18MPS 1500 TSRA BKN015CB", flightRules: .mvfr), type: .temporaryForecast)],
-            temperature: Temperature(value: 14, unit: .celsius),
-            dewPoint: Temperature(value: 8, unit: .celsius),
-            remarks: "TEST REMARKS",
-            metarString: metarString,
-            flightRules: .mvfr
-        )
-
-        try XCTAssertEqual(XCTUnwrap(metar), XCTUnwrap(testMETAR))
+        try compareMETAR("KRDU COR 281151Z AUTO 22007KT 9SM SCT040TCU FEW080 FEW250 22/20 SHRA BLU A3004 NOSIG RMK AO2 SLP166 60000 T02170200 10222 20206 53022", "KRDU", 28, 11, 51, wind: Wind(direction: 220, speed: .init(value: 7, unit: .knots)), qnh: QNH(value: 30.04, unit: .inchesOfMercury), cloudLayers: [.init(coverage: .scattered, height: .init(value: 4000, unit: .feet), significantCloudType: .toweringCumulus), .init(coverage: .few, height: .init(value: 8000, unit: .feet)), .init(coverage: .few, height: .init(value: 25000, unit: .feet))], visibility: .init(value: 9, unit: .miles), weather: [Weather(modifier: .moderate, phenomena: [.showers, .rain])], militaryColourCode: .blue, temperature: Temperature(value: 22, unit: .celsius), dewPoint: Temperature(value: 20, unit: .celsius), automaticStation: true, correction: true, noSignificantChangesExpected: true, remarks: "AO2 SLP166 60000 T02170200 10222 20206 53022", flightRules: .vfr)
+        try compareMETAR("EGGD 121212Z ///010 10SM", "EGGD", 12, 12, 12, cloudLayers: [.init(coverage: .notReported, height: .init(value: 1000, unit: .feet))], visibility: .init(value: 10, unit: .miles), flightRules: nil)
+        try compareMETAR("EGGD 121212Z ///010 1 1/4SM", "EGGD", 12, 12, 12, cloudLayers: [.init(coverage: .notReported, height: .init(value: 1000, unit: .feet))], visibility: .init(value: 1.25, unit: .miles), flightRules: nil)
+        try compareMETAR("PGUA 160631Z 33024G55KT 0SM +RA VV000 23/22 A2891 RMK WR//=", "PGUA", 16, 6, 31, wind: Wind(direction: 330, speed: .init(value: 24, unit: .knots), gustSpeed: .init(value: 55, unit: .knots)), qnh: QNH(value: 28.91, unit: .inchesOfMercury), cloudLayers: [.init(coverage: .skyObscured, height: .init(value: 0, unit: .feet))], visibility: .init(value: 0, unit: .miles), weather: [.init(modifier: .heavy, phenomena: [.rain])], temperature: .init(value: 23, unit: .celsius), dewPoint: .init(value: 22, unit: .celsius), remarks: "WR//=", flightRules: .lifr)
+        try compareMETAR("EGGD 251250Z AUTO 25016G27KT 220V280 9999 BKN019///TCU 11/11 Q1013", "EGGD", 25, 12, 50, wind: Wind(direction: 250, speed: .init(value: 16, unit: .knots), gustSpeed: .init(value: 27, unit: .knots), variation: .init(from: 220, to: 280)),qnh: QNH(value: 1013, unit: .hectopascals), cloudLayers: [.init(coverage: .broken, height: .init(value: 1900, unit: .feet), significantCloudType: .toweringCumulus)], visibility: .init(value: 10, unit: .kilometers, greaterThanOrEqual: true), temperature: .init(value: 11, unit: .celsius), dewPoint: .init(value: 11, unit: .celsius), automaticStation: true, flightRules: .mvfr)
+        try compareMETAR("UUDD 261100Z 25007MPS 200V290 9999 BKN026 14/08 Q0997 R88/290095 TEMPO 28012G18MPS 1500 TSRA BKN015CB RMK TEST REMARKS", "UUDD", 26, 11, 0, wind: Wind(direction: 250, speed: Wind.Speed(value: 7, unit: .metersPerSecond), gustSpeed: nil, variation: Wind.Variation(from: 200, to: 290)), qnh: QNH(value: 997, unit: .hectopascals), cloudLayers: [CloudLayer(coverage: .broken, height: .init(value: 2600, unit: .feet))], visibility: Visibility(value: 10, unit: .kilometers, greaterThanOrEqual: true), trends: [.init(metarRepresentation: .init(identifier: "UUDD", date: date(day: 26, hour: 11, minute: 00), wind: Wind(direction: 280, speed: .init(value: 12, unit: .metersPerSecond), gustSpeed: .init(value: 18, unit: .metersPerSecond)), cloudLayers: [.init(coverage: .broken, height: .init(value: 1500, unit: .feet), significantCloudType: .cumulonimbus)], visibility: .init(value: 1500, unit: .meters), weather: [.init(modifier: .moderate, phenomena: [.thunderstorm, .rain])], metarString: "28012G18MPS 1500 TSRA BKN015CB", flightRules: .mvfr), type: .temporaryForecast)], temperature: Temperature(value: 14, unit: .celsius), dewPoint: Temperature(value: 8, unit: .celsius), remarks: "TEST REMARKS", flightRules: .mvfr)
     }
 
     func testLIFRVisibility() throws {
@@ -126,7 +83,7 @@ final class METARTests: XCTestCase {
         try XCTAssertEqual(XCTUnwrap(METAR(rawMETAR: "EGGD 121212Z 9999")).ceilingAndVisibilityOK, false)
     }
 
-    private func compareMETAR(_ rawMETAR: String, _ identifier: String, _ day: Int, _ hour: Int, _ minute: Int, wind: Wind? = nil, qnh: QNH? = nil, skyCondition: SkyCondition? = nil, cloudLayers: [CloudLayer] = [], visibility: Visibility? = nil, weather: [Weather] = [], trends: [Forecast] = [], militaryColourCode: MilitaryColourCode? = nil, temperature: Temperature? = nil, dewPoint: Temperature? = nil, ceilingAndVisibilityOK: Bool = false, automaticStation: Bool = false, correction: Bool = false, noSignificantChangesExpected: Bool = false, remarks: String? = nil, flightRules: NOAAFlightRules? = nil) throws {
+    private func compareMETAR(_ rawMETAR: String, _ identifier: String, _ day: Int, _ hour: Int, _ minute: Int, wind: Wind? = nil, qnh: QNH? = nil, skyCondition: SkyCondition? = nil, cloudLayers: [CloudLayer] = [], visibility: Visibility? = nil, weather: [Weather] = [], trends: [Forecast] = [], militaryColourCode: MilitaryColourCode? = nil, temperature: Temperature? = nil, dewPoint: Temperature? = nil, ceilingAndVisibilityOK: Bool = false, automaticStation: Bool = false, correction: Bool = false, noSignificantChangesExpected: Bool = false, remarks: String? = nil, flightRules: NOAAFlightRules?) throws {
         try XCTAssertEqual(XCTUnwrap(METAR(rawMETAR: rawMETAR)), XCTUnwrap(METAR(
             identifier: identifier,
             date: date(day: day, hour: hour, minute: minute),
